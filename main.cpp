c06641b04af0e490e878e09fb607b73f991e51c1
Jack Honour <JaxkDev@gmail.com>
2024-05-07 22:15:59 +0100

    OpenGL Template.

--- main.cpp
+++ main.cpp
@@ -1,60 +1,53 @@
-#include <iostream>
+#include <cstdint>
+#include <cassert>
 #include <SDL.h>
+#include <SDL_opengl.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv []) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    //DEMO WINDOW
-    SDL_Window *win = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_BORDERLESS);
-    if (win == nullptr) {
-        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-    SDL_HideWindow(win);
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    if (ren == nullptr) {
-        SDL_DestroyWindow(win);
-        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int32_t b32;
 
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++){
-            SDL_SetRenderDrawColor(ren, rand() % 255, rand() % 255, rand() % 255, 255);
-            SDL_Rect r;
-            r.x = 50*i;
-            r.y = 50*j;
-            r.w = 50;
-            r.h = 50;
-            SDL_RenderFillRect(ren, &r);
-            //SDL_RenderPresent(ren);// - Show individual draws.
-        }
-    }
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING, "Warning!", "Failed to do x y z.", win);
-    SDL_ShowWindow(win);
-    SDL_RenderPresent(ren);
+#define WinWidth 550
+#define WinHeight 400
 
-    SDL_Event e;
-    bool quit = false;
-    while (!quit){
-        while (SDL_PollEvent(&e)){
-            if (e.type == SDL_QUIT){
-                quit = true;
-            }
-            if (e.type == SDL_KEYDOWN){
-                if (e.key.keysym.sym == SDLK_ESCAPE){
-                    quit = true;
+int main(int ArgCount, char **Args) {
+    u32 WindowFlags = SDL_WINDOW_OPENGL;
+    SDL_Window *Window = SDL_CreateWindow("OpenGL Test", 100, 100, WinWidth, WinHeight, WindowFlags);
+    assert(Window);
+    SDL_GLContext Context = SDL_GL_CreateContext(Window);
+
+    b32 Running = 1;
+    b32 FullScreen = 0;
+    while (Running) {
+        SDL_Event Event;
+        while (SDL_PollEvent(&Event)) {
+            if (Event.type == SDL_KEYDOWN) {
+                switch (Event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        Running = 0;
+                        break;
+                    case 'f':
+                        FullScreen = !FullScreen;
+                        if (FullScreen) {
+                            SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
+                        } else {
+                            SDL_SetWindowFullscreen(Window, WindowFlags);
+                        }
+                        break;
+                    default:
+                        break;
                 }
+            } else if (Event.type == SDL_QUIT) {
+                Running = 0;
             }
         }
+
+        glViewport(0, 0, WinWidth, WinHeight);
+        glClearColor(1.f, 0.f, 0.f, 0.5f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(Window);
     }
 
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
     return 0;
 }
